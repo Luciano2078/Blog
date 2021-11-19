577644676393410e9168f7beae4484e27dba3447
Luciano2078 <luciano.pereiraaristimunho@gmail.com>
2021-11-19 19:41:45 -0300

    models

--- aplicativo/models.py
+++ aplicativo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+STATUS = (
+    (0, "Draft"),
+    (0, "Publish"),
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices= STATUS, default=0)
+		
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.title
--- projeto/settings.py
+++ projeto/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'aplicativo'
+    'aplicativo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
