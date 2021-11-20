6df7707264873d9c3f677efa608fd2c536008b76
Luciano2078 <luciano.pereiraaristimunho@gmail.com>
2021-11-19 21:20:22 -0300

    views

--- aplicativo/views.py
+++ aplicativo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
+
+from .models import Post
+
+
+
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+
